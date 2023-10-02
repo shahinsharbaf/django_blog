c82ad440f412571881a35fabb1a9b00af6552aab
shahinsharbaf <shahin.sharbaf7@gmail.com>
2023-10-02 10:53:56 +0330

    modefied

--- authenticate/models.py
+++ authenticate/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class signup(models.Model):
+    username = models.CharField(max_length=40)
+    password = models.CharField(max_length=40)
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.username
--- authenticate/views.py
+++ authenticate/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        user_name = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+
+    if User.objects.filter(username=user_name).exists():
+        messages.info(request,'username exist')
+    else:
+        User.objects.create_user(username=user_name,password=password,email=email)
+
+    return render(request,'authenticate/signup.html')
